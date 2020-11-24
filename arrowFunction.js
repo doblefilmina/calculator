f1c04d7a52c88511778c9d6f8a72b49756f733a6
doblefilmina <brusafederico@gmail.com>
2020-11-24 16:26:29 -0300

    regular and arrow functions

---
+++ arrowFunction.js
@@ -0,0 +1,15 @@
+console.log('with arrow functions')
+
+const sumA = (a,b) =>  a+b
+const subA = (a,b) => a-b
+const mulA = (a,b) => a*b
+const divA = (a,b) =>a/b
+
+console.log('sum(1,2)')
+console.log(sumA(1,2))
+
+console.log('div(6,3)')
+console.log(divA(6,3))
+
+console.log('mul(2,2)')
+console.log(mulA(2,2))
---
+++ regularFunctions.js
@@ -0,0 +1,26 @@
+console.log('with regular functions')
+
+function sum(a,b) {
+  return a+b
+}
+
+function sub(a,b) {
+  return a - b
+}
+
+function mul(a, b) {
+  return a * b
+}
+
+function div(a,b) {
+  return a/b
+}
+
+console.log('sum(1,2)')
+console.log(sum(1,2))
+
+console.log('div(6,3)')
+console.log(div(6,3))
+
+console.log('mul(2,2)')
+console.log(mul(2,2))
